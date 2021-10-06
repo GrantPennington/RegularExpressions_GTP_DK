23400dc2c081e29d05a519a795e7a0a177ee45ff
GrantPennington <gtpenn16@gmail.com>
2021-10-05 20:52:42 -0400

    removed unused code

--- Part_Two/ExtractText.java
+++ Part_Two/ExtractText.java
@@ -19,18 +19,16 @@ public class ExtractText {
         try (PDDocument document = PDDocument.load(new File("/home/cs322/Documents/RegularExpressions/Part_Two/Bellarmine_Schedule.pdf"))) { // input the PDF through the command line
             document.getClass();
             File plainText = new File("/home/cs322/Documents/RegularExpressions/Part_Two/Bellarmine_Schedule.txt");
-            FileWriter writer = new FileWriter(plainText);
+            FileWriter writer = new FileWriter(plainText); // new FileWriter object being passed the plainText File object
             if (!document.isEncrypted()) {
-
                 PDFTextStripper tStripper = new PDFTextStripper();
                 String pdfFileInText = tStripper.getText(document);
                 String lines[] = pdfFileInText.split("\\r?\\n");
                 
                 for (String line : lines) {
-                    writer.write(line);
-                    //writer.close();
+                    writer.write(line); // write each line to a text file
                 }
-                writer.close();
+                writer.close(); //close the FileWriter
                 System.out.println("Text file created");
             }
         } catch (IOException e) {
